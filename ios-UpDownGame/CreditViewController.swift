d02cac6cec288ef26c69d4fa899ca146b5a07dc0
Sookim18 <scstnghks@naver.com>
2021-03-16 23:36:08 +0900

    Feat : 크레딧화면 기본 구성

---
+++ ios-UpDownGame/CreditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreditViewController.swift
+//  ios-UpDownGame
+//
+//  Created by sookim on 2021/03/16.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
